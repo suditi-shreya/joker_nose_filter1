5897ded590d60c18aa20e8fc24237895528c9a2f
suditi-shreya <81765360+suditi-shreya@users.noreply.github.com>
2021-12-02 19:44:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload(){
+
+}
+
+function setup(){
+canvas=createCanvas(300,300);
+canvas.center();
+video=createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+posenet= ml5.poseNet(video , modelloaded);
+poseNet.on('pose',gotposes);
+
+}
+function modelloaded(){
+    console.log("posenet is loaded");
+}
+
+function gotposes(results){
+    if(results>0){
+        console.log(results);
+        console.log("nose x"+results[0].pose.nose.x);
+        console.log("nose y"+results[0].pose.nose.y);
+    } 
+    }
+
+function draw(){
+image(video,0,0,300,300);
+
+}
+function snapshot(){
+    save("name.png")
+}
